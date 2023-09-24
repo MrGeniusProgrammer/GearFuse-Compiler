f4cd1a75a120d283d247f31ec4196353a3bce742
MrGeniusProgrammer <mrgeniusprogrammer69@gmail.com>
2023-09-24 21:01:46 +0300

    Reverted back to using float and int class

--- compiler/Settings/include.h
+++ compiler/Settings/include.h
@@ -9,3 +9,9 @@
 std::unique_ptr<llvm::LLVMContext> context(new llvm::LLVMContext());
 std::unique_ptr<llvm::IRBuilder<>> builder(new llvm::IRBuilder<>(*context));
 std::unique_ptr<llvm::Module> module(new llvm::Module("main.gc", *context));
+
+static llvm::AllocaInst *CreateEntryBlockAlloca(llvm::Function *function, llvm::Type *type, llvm::StringRef var_name)
+{
+  llvm::IRBuilder<> tem_builder(&function->getEntryBlock(), function->getEntryBlock().begin());
+  return tem_builder.CreateAlloca(type, nullptr, var_name);
+}
--- compiler/main.cpp
+++ compiler/main.cpp
@@ -85,23 +85,21 @@ public:
     ASTCallExpressionID,
     ASTStringExpressionID,
     ASTCharExpressionID,
-    ASTNumberExpressionID,
-    ASTIntExpressionID,
-    ASTFloatExpressionID,
+    ASTIntNumberExpressionID,
+    ASTFloatNumberExpressionID,
     ASTIdentifierExpressionID,
     ASTFucntionID,
     ASTPrototypeID,
-    ASTStatementID,
     ASTReturnStatementID,
-    ASTVariableExpressionID,
-    ASTAssignVariableExpressionID,
-    ASTMutateVariableExpressionID,
-    ASTForExpressionID,
-    ASTIfExpressionID,
-    ASTWhileExpressionID,
-    ASTDoWhileExpressionID,
-    ASTElseExpressionID,
-    ASTElseIfExpressionID,
+    ASTVariableStatementID,
+    ASTAssignVariableStatementID,
+    ASTMutateVariableStatementID,
+    ASTForStatementID,
+    ASTIfStatementID,
+    ASTWhileStatementID,
+    ASTDoWhileStatementID,
+    ASTElseStatementID,
+    ASTElseIfStatementID,
   };
 
   std::string showKind;
@@ -120,14 +118,21 @@ public:
   ASTNodeID getASTNodeID() { return ID; }
 };
 
-class ASTExpression : public ASTNode
+class ASTStatement : public ASTNode
+{
+public:
+  ASTStatement(ASTNodeID ID, std::string showKind = "", std::string showValue = "") : ASTNode(ID, showKind, showValue){};
+};
+
+class ASTExpression : public ASTStatement
 {
 protected:
   Type *type;
 
+  explicit ASTExpression(ASTNodeID ID, std::string showKind = "", std::string showValue = "") : ASTStatement(ID, showKind, showValue){};
+  explicit ASTExpression(Type *type, ASTNodeID ID, std::string showKind = "", std::string showValue = "") : type(type), ASTStatement(ID, showKind, showValue){};
+
 public:
-  ASTExpression(ASTNodeID ID, std::string showKind = "", std::string showValue = "") : ASTNode(ID, showKind, showValue){};
-  ASTExpression(Type *type, ASTNodeID ID, std::string showKind = "", std::string showValue = "") : type(type), ASTNode(ID, showKind, showValue){};
   Type *getType() { return type; }
   virtual void setType(Type *type) { this->type = type; }
   virtual void evaluateType(){};
@@ -137,21 +142,37 @@ class ASTNumberExpression : public ASTExpression
 {
 protected:
   Token token;
+  explicit ASTNumberExpression(Token token, Type *type, ASTNodeID ID, std::string showKind) : token(token), ASTExpression(type, ID, showKind, token.value){};
 
 public:
-  ASTNumberExpression(Token token, Type *type) : token(token), ASTExpression(type, ASTNodeID::ASTNumberExpressionID, "NumberExpression", token.value){};
-  llvm::Value *codegen() override
+  void setType(Type *type) override
   {
-    if (getType()->isIntegerTy())
+    if (!(type->isNumberTy()))
     {
-      return llvm::ConstantInt::get(getType()->getLLVMTy(), std::stol(token.value), true);
-    }
-    else if (getType()->isFloatTy())
-    {
-      return llvm::ConstantFP::get(getType()->getLLVMTy(), std::stof(token.value));
+      return;
     }
 
-    return nullptr;
+    this->type = type;
+  }
+};
+
+class ASIntTNumberExpression : public ASTNumberExpression
+{
+public:
+  ASIntTNumberExpression(Token token) : ASTNumberExpression(token, Type::getInteger32Ty(), ASTNode::ASTIntNumberExpressionID, "IntNumberExpression"){};
+  llvm::Value *codegen() override
+  {
+    return llvm::ConstantInt::get(getType()->getLLVMTy(), std::stol(token.value), true);
+  }
+};
+
+class ASFloatTNumberExpression : public ASTNumberExpression
+{
+public:
+  ASFloatTNumberExpression(Token token) : ASTNumberExpression(token, Type::getFloat32Ty(), ASTNode::ASTFloatNumberExpressionID, "FloatNumberExpression"){};
+  llvm::Value *codegen() override
+  {
+    return llvm::ConstantFP::get(getType()->getLLVMTy(), std::stof(token.value));
   }
 };
 
@@ -159,21 +180,21 @@ class ASTBinaryExpression : public ASTExpression
 {
 protected:
   Token operatorToken;
-  std::unique_ptr<ASTExpression> leftOperand;
-  std::unique_ptr<ASTExpression> rightOperand;
+  ASTExpression *leftOperand;
+  ASTExpression *rightOperand;
 
 public:
   ASTBinaryExpression(Token operatorToken,
-                      std::unique_ptr<ASTExpression> leftOperand,
-                      std::unique_ptr<ASTExpression> rightOperand) : operatorToken(operatorToken),
-                                                                     leftOperand(std::move(leftOperand)),
-                                                                     rightOperand(std::move(rightOperand)),
-                                                                     ASTExpression(ASTNodeID::ASTBinaryExpressionID, "BinaryExpression", operatorToken.value){};
+                      ASTExpression *leftOperand,
+                      ASTExpression *rightOperand) : operatorToken(operatorToken),
+                                                     leftOperand(leftOperand),
+                                                     rightOperand(rightOperand),
+                                                     ASTExpression(ASTNodeID::ASTBinaryExpressionID, "BinaryExpression", operatorToken.value){};
   std::vector<ASTNode *> getChildrenShow() override
   {
     std::vector<ASTNode *> children;
-    children.push_back(&(*leftOperand));
-    children.push_back(&(*rightOperand));
+    children.push_back(leftOperand);
+    children.push_back(rightOperand);
     return std::move(children);
   }
 
@@ -287,17 +308,17 @@ class ASTUnaryExpression : public ASTExpression
 {
 private:
   Token operatorToken;
-  std::unique_ptr<ASTExpression> operand;
+  ASTExpression *operand;
 
 public:
   ASTUnaryExpression(Token operatorToken,
-                     std::unique_ptr<ASTExpression> operand) : operatorToken(operatorToken),
-                                                               operand(std::move(operand)),
-                                                               ASTExpression(ASTNode::ASTUnaryExpressionID, "UnaryExpression", operatorToken.value){};
+                     ASTExpression *operand) : operatorToken(operatorToken),
+                                               operand(operand),
+                                               ASTExpression(ASTNode::ASTUnaryExpressionID, "UnaryExpression", operatorToken.value){};
   std::vector<ASTNode *> getChildrenShow() override
   {
     std::vector<ASTNode *> children;
-    children.push_back(&(*operand));
+    children.push_back(operand);
     return std::move(children);
   }
 
@@ -342,6 +363,201 @@ public:
   }
 };
 
+class ASTVariableStatement : public ASTStatement
+{
+protected:
+  Token token;
+  Type *type;
+  llvm::AllocaInst *value;
+
+public:
+  ASTVariableStatement(Token token,
+                       Type *type,
+                       ASTNodeID ID = ASTNode::ASTVariableStatementID,
+                       std::string showKind = "VariableStatement") : token(token),
+                                                                     type(type),
+                                                                     ASTStatement(ID, showKind, token.value){};
+
+  std::string getName() { return token.value; }
+  Type *getType() { return type; }
+  llvm::AllocaInst *getAlocatedValue() { return value; }
+  virtual void evaluateType(){};
+
+  virtual llvm::Value *codegen() override
+  {
+    llvm::Function *function = builder->GetInsertBlock()->getParent();
+    value = CreateEntryBlockAlloca(function, type->getLLVMTy(), token.value.c_str());
+    return value;
+  }
+};
+
+ASTVariableStatement *currentVariable;
+class ASTBlock : public ASTNode
+{
+protected:
+  std::vector<ASTStatement *> body;
+  std::map<std::string, ASTVariableStatement *> namedVariables;
+
+public:
+  ASTBlock(std::vector<ASTStatement *> body, std::string showKind = "Block") : body(std::move(body)), ASTNode(ASTNode::ASTBlockID, showKind){};
+  ASTBlock(std::string showKind = "Block") : ASTNode(ASTNode::ASTBlockID, showKind){};
+
+  std::vector<ASTNode *> getChildrenShow() override
+  {
+    std::vector<ASTNode *> children;
+    // printf("------ DEBUGING ------");
+    for (int i = 0; i < body.size(); i++)
+    {
+      children.push_back(body[i]);
+    }
+
+    return std::move(children);
+  }
+
+  llvm::Value *codegen() override;
+  void newNamedVariable(ASTVariableStatement *variable)
+  {
+    namedVariables[variable->getName()] = variable;
+    currentVariable = variable;
+  }
+
+  ASTVariableStatement *namedVariable(std::string variableName)
+  {
+    currentVariable = namedVariables[variableName];
+    return currentVariable;
+  }
+
+  void pushStatement(ASTStatement *statement)
+  {
+    body.push_back(statement);
+  }
+};
+
+class BlockStack
+{
+protected:
+  std::vector<ASTBlock *> blocks;
+
+public:
+  BlockStack() = default;
+  ASTBlock *getCurrentBlock() { return blocks.back(); }
+  void pushBlock(ASTBlock *block) { blocks.push_back(block); }
+  void popBlock() { blocks.pop_back(); }
+
+  ASTVariableStatement *namedVariable(std::string variableName)
+  {
+    for (int i = blocks.size() - 1; i >= 0; i--)
+    {
+      ASTVariableStatement *variable = blocks[i]->namedVariable(variableName);
+      if (variable)
+      {
+        return variable;
+      }
+    }
+
+    return nullptr;
+  }
+};
+
+BlockStack *globalBlockStack(new BlockStack());
+llvm::Value *ASTBlock::codegen()
+{
+  globalBlockStack->pushBlock(this);
+
+  llvm::Value *FnIR;
+  for (int i = 0; i < body.size(); i++)
+  {
+    FnIR = body[i]->codegen();
+  }
+
+  globalBlockStack->popBlock();
+
+  return FnIR;
+}
+
+class ASTAssignVariableStatement : public ASTVariableStatement
+{
+protected:
+  ASTExpression *expression;
+
+public:
+  ASTAssignVariableStatement(ASTExpression *expression,
+                             Token token,
+                             Type *type,
+                             ASTNodeID ID = ASTNode::ASTAssignVariableStatementID,
+                             std::string showKind = "AssignVariableStatement") : expression(expression),
+                                                                                 ASTVariableStatement(token, type, ID, showKind)
+  {
+    globalBlockStack->getCurrentBlock()->newNamedVariable(this);
+  };
+
+  virtual void evaluateType() override
+  {
+    if (!getType()->isEquals(expression->getType()))
+    {
+      expression->setType(getType());
+    }
+  }
+
+  std::vector<ASTNode *> getChildrenShow() override
+  {
+    std::vector<ASTNode *> children;
+    children.push_back(expression);
+    return std::move(children);
+  }
+
+  virtual llvm::StoreInst *codegen() override
+  {
+    llvm::Value *expressionValue = expression->codegen();
+
+    if (!expressionValue)
+    {
+      return nullptr;
+    }
+
+    llvm::Function *function = builder->GetInsertBlock()->getParent();
+    value = CreateEntryBlockAlloca(function, type->getLLVMTy(), token.value);
+    return builder->CreateStore(expressionValue, value);
+  }
+};
+
+class ASTIdentifierExpression : public ASTExpression
+{
+protected:
+  Token token;
+  ASTVariableStatement *foundVariable;
+
+public:
+  ASTIdentifierExpression(Token token) : token(token), ASTExpression(ASTNode::ASTIdentifierExpressionID, "IdentifierExpression", token.value)
+  {
+    foundVariable = globalBlockStack->namedVariable(token.value);
+    if (foundVariable)
+    {
+      setType(foundVariable->getType());
+    }
+  };
+
+  llvm::LoadInst *codegen() override
+  {
+    if (!foundVariable)
+    {
+      return nullptr;
+    }
+
+    llvm::AllocaInst *value = foundVariable->getAlocatedValue();
+    if (!value)
+    {
+      return nullptr;
+    }
+
+    return builder->CreateLoad(value->getAllocatedType(), value, token.value.c_str());
+  }
+};
+
+class ASTCallExpression : public ASTExpression
+{
+};
+
 void PrettyPrint(ASTNode *node, std::string indent = "", bool is_last = true)
 {
   if (node == nullptr)
@@ -377,18 +593,38 @@ int main()
   // testTys.push_back(type3);
   // TestTypes(std::move(testTys));
 
-  std::unique_ptr<ASTNumberExpression> intExpr1(new ASTNumberExpression(Token("4", Token::Type::LITERAL_INT), Type::getInteger32Ty()));
-  std::unique_ptr<ASTNumberExpression> intExpr2(new ASTNumberExpression(Token("2", Token::Type::LITERAL_INT), Type::getInteger32Ty()));
-  std::unique_ptr<ASTNumberExpression> intExpr3(new ASTNumberExpression(Token("2", Token::Type::LITERAL_INT), Type::getInteger32Ty()));
-  std::unique_ptr<ASTUnaryExpression> unaryExpr1(new ASTUnaryExpression(Token("-", Token::Type::HYPHEN), std::move(intExpr3)));
+  // binaryExpr2->evaluateType();
+  // PrettyPrint(binaryExpr2);
+  // llvm::Value *value = binaryExpr2->codegen();
+  // value->print(llvm::outs());
 
-  std::unique_ptr<ASTBinaryExpression> binaryExpr1(new ASTBinaryExpression(Token("+", Token::Type::PLUS), std::move(intExpr1), std::move(intExpr2)));
-  std::unique_ptr<ASTBinaryExpression> binaryExpr2(new ASTBinaryExpression(Token("*", Token::Type::ASTERISK), std::move(unaryExpr1), std::move(binaryExpr1)));
+  llvm::Function *function = llvm::Function::Create(llvm::FunctionType::get(builder->getInt32Ty(), false), llvm::GlobalValue::ExternalLinkage, "main", module.get());
+  llvm::BasicBlock *basicBlock = llvm::BasicBlock::Create(*context, "entry", function);
+  builder->SetInsertPoint(basicBlock);
 
-  binaryExpr2->evaluateType();
-  PrettyPrint(&(*binaryExpr2));
-  llvm::Value *value = binaryExpr2->codegen();
-  value->print(llvm::outs());
+  ASTBlock *block1(new ASTBlock("ProgramBlock"));
+  globalBlockStack->pushBlock(block1);
+
+  ASIntTNumberExpression *intExpr4(new ASIntTNumberExpression(Token("2", Token::Type::LITERAL_INT)));
+  ASTAssignVariableStatement *variableStm1(new ASTAssignVariableStatement(intExpr4, Token("a", Token::Type::IDENTIFIER), Type::getInteger32Ty()));
+  block1->pushStatement(variableStm1);
+
+  ASIntTNumberExpression *intExpr1(new ASIntTNumberExpression(Token("4", Token::Type::LITERAL_INT)));
+  ASIntTNumberExpression *intExpr2(new ASIntTNumberExpression(Token("2", Token::Type::LITERAL_INT)));
+  ASIntTNumberExpression *intExpr3(new ASIntTNumberExpression(Token("2", Token::Type::LITERAL_INT)));
+  ASTUnaryExpression *unaryExpr1(new ASTUnaryExpression(Token("-", Token::Type::HYPHEN), intExpr3));
+  ASTIdentifierExpression *identifierExpr1(new ASTIdentifierExpression(Token("a", Token::Type::IDENTIFIER)));
+
+  ASTBinaryExpression *binaryExpr1(new ASTBinaryExpression(Token("+", Token::Type::PLUS), intExpr1, intExpr2));
+  ASTBinaryExpression *binaryExpr2(new ASTBinaryExpression(Token("*", Token::Type::ASTERISK), unaryExpr1, binaryExpr1));
+  ASTBinaryExpression *binaryExpr3(new ASTBinaryExpression(Token("-", Token::Type::HYPHEN), identifierExpr1, binaryExpr2));
+
+  block1->pushStatement(binaryExpr3);
+  // llvm::Value *value = binaryExpr3->codegen();
+  // value->print(llvm::outs());
+
+  globalBlockStack->popBlock();
+  PrettyPrint(block1);
 
   return 1;
 }
